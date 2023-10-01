112a2d1470abf12e59e1ce81d3206ae18b24ba00
Ilyas <rashidov.ilias@gmail.com>
2023-10-02 00:05:02 +0300

    finish

--- src/main/kotlin/com/test/plugins/Routing.kt
+++ src/main/kotlin/com/test/plugins/Routing.kt
@@ -1,6 +1,9 @@
 package com.test.plugins
 
 import com.test.routes.customerRoutes
+import com.test.routes.getOrderRoute
+import com.test.routes.listOrderRoute
+import com.test.routes.totalizeOrderRoute
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -8,5 +11,9 @@ import io.ktor.server.routing.*
 fun Application.configureRouting() {
     routing {
         customerRoutes()
+        listOrderRoute()
+        getOrderRoute()
+        totalizeOrderRoute()
+
     }
 }
